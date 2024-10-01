029bf525dd121d5c48b9f03b0a6b1cf69f73b366
eduardoJunior09 <eduardojunior.dev@gmail.com>
2024-09-30 21:45:37 -0300

    refactor: altera nomeclatura do componente

--- lista-de-compras/src/app/components/new-item/new-item.component.spec.ts
+++ lista-de-compras/src/app/components/new-item/new-item.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TaskComponent } from './task.component';
+import { NewItemComponent } from './new-item.component';
 
-describe('TaskComponent', () => {
-  let component: TaskComponent;
-  let fixture: ComponentFixture<TaskComponent>;
+describe('NewItemComponent', () => {
+  let component: NewItemComponent;
+  let fixture: ComponentFixture<NewItemComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TaskComponent]
+      imports: [NewItemComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(TaskComponent);
+    fixture = TestBed.createComponent(NewItemComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
