7ac32ffd07c1a04680a122abb4eaef2a77d7ccaf
eduardoJunior09 <eduardojunior.dev@gmail.com>
2024-10-01 21:55:37 -0300

    recator: adiciona lista interativa apartir da inserção de dados do usuário

--- lista-de-compras/src/app/features/item/item.component.ts
+++ lista-de-compras/src/app/features/item/item.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { EditButtonComponent } from '../../shared/buttons/edit-button/edit-button.component';
 import { RemoveButtonComponent } from '../../shared/buttons/remove-button/remove-button.component';
+import { TaskService } from './../../service/task.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-item',
   standalone: true,
-  imports: [MatCheckboxModule, EditButtonComponent, RemoveButtonComponent],
+  imports: [MatCheckboxModule, EditButtonComponent, RemoveButtonComponent, CommonModule],
   templateUrl: './item.component.html',
-  styleUrl: './item.component.scss'
+  styleUrl: './item.component.scss',
 })
 export class ItemComponent {
-  typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
+  itens: { title: string; completed: boolean }[] = [];
+
+  constructor(private taskService: TaskService) {
+    this.itens = this.taskService.getItens();
+  }
+
+  removeTask(index: number) {
+    this.taskService.removeItem(index);
+  }
+
+  completeTask(index: number) {
+    this.taskService.completeItens(index);
+  }
 }
