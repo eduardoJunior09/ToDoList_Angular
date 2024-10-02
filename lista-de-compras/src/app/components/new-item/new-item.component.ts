b75579cabe78cbbbf87421a40e231756ec1d519d
eduardoJunior09 <eduardojunior.dev@gmail.com>
2024-10-01 21:56:25 -0300

    refactor: adiciona função que controla e valida as entradas de dados para criação da lista

--- lista-de-compras/src/app/components/new-item/new-item.component.ts
+++ lista-de-compras/src/app/components/new-item/new-item.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TaskService } from './../../service/task.service';
 
 @Component({
   selector: 'app-new-item',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './new-item.component.html',
-  styleUrl: './new-item.component.scss'
+  styleUrls: ['./new-item.component.scss'],
 })
 export class NewItemComponent {
+  newItem: string = '';
+  showError: boolean = false;
 
+  constructor(private taskService: TaskService) {}
+
+  addItem() {
+    this.validateForm();
+    if (!this.showError) {
+      this.taskService.addItem(this.newItem.trim());
+      this.newItem = '';
+    }
+  }
+
+  validateForm() {
+    this.showError = this.newItem.trim().length < 2;
+  }
 }
