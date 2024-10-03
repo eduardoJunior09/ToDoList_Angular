a0b84609c0b30f264037c283e781a684367f5d65
eduardoJunior09 <eduardojunior.dev@gmail.com>
2024-10-03 20:39:16 -0300

    feat: adiciona componentes de itens pendentes e concluídos ao DropDownListComponent

--- lista-de-compras/src/app/features/drop-down-list/drop-down-list.component.ts
+++ lista-de-compras/src/app/features/drop-down-list/drop-down-list.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import {MatExpansionModule} from '@angular/material/expansion';
-import { ItemComponent } from './../item/item.component';
+import { TaskService } from './../../service/task.service';
+import { ItemCompletedComponent } from '../item-completed/item-completed.component';
+import { ItemPendingComponent } from '../item-pending/item-pending.component';
 
 
 @Component({
   selector: 'app-drop-down-list',
   standalone: true,
-  imports: [MatExpansionModule, ItemComponent],
+  imports: [MatExpansionModule, ItemCompletedComponent, ItemPendingComponent],
 
 templateUrl: './drop-down-list.component.html',
   styleUrl: './drop-down-list.component.scss'
 })
 export class DropDownListComponent {
- 
+  
 }
