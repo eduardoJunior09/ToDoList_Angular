8fd246e6791b0e5cbaf64462e06fdb4e8ac13c8f
eduardoJunior09 <eduardojunior.dev@gmail.com>
2024-10-08 09:56:56 -0300

    feat: adciona funcionalidade de editar item

--- src/app/features/item-pending/item-pending.component.ts
+++ src/app/features/item-pending/item-pending.component.ts
@@ -1,26 +1,30 @@
-import { Component } from '@angular/core';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { EditButtonComponent } from '../../shared/buttons/edit-button/edit-button.component';
-import { RemoveButtonComponent } from '../../shared/buttons/remove-button/remove-button.component';
-import { CommonModule } from '@angular/common';
-import { TaskService } from '../../service/task.service';
+import { Component } from "@angular/core";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { EditButtonComponent } from "../../shared/buttons/edit-button/edit-button.component";
+import { RemoveButtonComponent } from "../../shared/buttons/remove-button/remove-button.component";
+import { CommonModule } from "@angular/common";
+import { TaskService } from "../../service/task.service";
+import { MatDialog, MatDialogModule } from "@angular/material/dialog";
+import { ModalUpdateComponent } from "./../../components/modal-update/modal-update.component";
 
 @Component({
-  selector: 'app-item-pending',
+  selector: "app-item-pending",
   standalone: true,
   imports: [
     MatCheckboxModule,
     EditButtonComponent,
     RemoveButtonComponent,
     CommonModule,
+    ModalUpdateComponent,
+    MatDialogModule,
   ],
-  templateUrl: './item-pending.component.html',
-  styleUrl: './item-pending.component.scss',
+  templateUrl: "./item-pending.component.html",
+  styleUrls: ["./item-pending.component.scss"],
 })
 export class ItemPendingComponent {
   itensPendentes: { title: string; completed: boolean }[] = [];
 
-  constructor(private taskService: TaskService) {
+  constructor(private taskService: TaskService, private dialog: MatDialog) {
     this.itensPendentes = this.taskService.getItens();
   }
 
@@ -36,5 +40,18 @@ export class ItemPendingComponent {
 
   updateItems() {
     this.itensPendentes = this.taskService.getItens();
+  }
+
+  editItem(index: number) {
+    const dialogRef = this.dialog.open(ModalUpdateComponent, {
+      data: { index, title: this.itensPendentes[index].title },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.taskService.updatetitle(index, result);
+        this.updateItems();
+      }
+    });
   }
 }
