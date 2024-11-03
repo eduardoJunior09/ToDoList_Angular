20f699c149249b4dc2e11314931a46aca5dbd295
eduardoJunior09 <eduardojunior.dev@gmail.com>
2024-11-03 14:20:13 -0300

    feat: implementar AuthGuard para proteger rotas autenticadas

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { tap, map } from 'rxjs/operators'; // Adicione o 'map' aqui
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +12,12 @@ export class AuthGuard implements CanActivate {
 
   canActivate(): Observable<boolean> {
     return this.auth.isAuthenticated$.pipe(
-      tap((isAuthenticated: boolean) => { // Definindo explicitamente o tipo
+      tap((isAuthenticated: boolean) => {
         if (!isAuthenticated) {
           this.router.navigate(['/']); // Redireciona para a página inicial se não estiver autenticado
         }
-      })
+      }),
+      map((isAuthenticated: boolean) => isAuthenticated) // Retorna o valor booleano
     );
   }
 }
