44882d2daf05abe0acb7ff063d40fbe1d7034d32
eduardoJunior09 <eduardojunior.dev@gmail.com>
2024-11-03 19:21:35 -0300

    refactor: adiciona nova logica para novas taferas com validação e integração com TaskService

--- src/app/components/new-item/new-item.component.ts
+++ src/app/components/new-item/new-item.component.ts
@@ -1,28 +1,42 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { TaskService } from './../../service/task.service';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { TaskService } from "./../../service/task.service";
+import { Task } from "./../../service/task";
 
 @Component({
-  selector: 'app-new-item',
+  selector: "app-new-item",
   standalone: true,
   imports: [FormsModule, CommonModule],
-  templateUrl: './new-item.component.html',
-  styleUrls: ['./new-item.component.scss'],
+  templateUrl: "./new-item.component.html",
+  styleUrls: ["./new-item.component.scss"],
 })
 export class NewItemComponent {
-  newItem: string = '';
-  showError: boolean = false;
-
   constructor(private taskService: TaskService) {}
+
+  newItem: string = "";
+  showError: boolean = false;
+  tasks = Array<Task>();
 
   addItem() {
     this.validateForm();
+
     if (!this.showError) {
-      this.taskService.addItem(this.newItem.trim());
-      this.newItem = '';
+      const taskToAdd:  Omit<Task, 'id'> = { title: this.newItem.trim(), completed: false };
+
+      this.taskService.addTask(taskToAdd).subscribe({
+        next: (task: Task) => {
+          this.tasks.push(task);
+          this.newItem = "";
+          console.log("Lista de Tarefas:", this.tasks);
+        },
+        error: (err) => {
+          console.error("Erro ao adicionar a tarefa:", err);
+        },
+      });
     }
   }
+
 
   validateForm() {
     this.showError = this.newItem.trim().length < 2;
