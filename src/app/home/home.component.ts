db5f002c0280627798a7c1545dcf5a254b369cf2
eduardoJunior09 <eduardojunior.dev@gmail.com>
2024-11-03 14:16:15 -0300

    feat: adicionar funcionalidade de login do usuário

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,20 @@
-
-import { Component } from '@angular/core';
-import { AuthService } from '@auth0/auth0-angular'; 
+import { Component } from "@angular/core";
+import { AuthService } from "@auth0/auth0-angular";
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   standalone: true,
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent {
-  constructor(public auth: AuthService) {} // Injete o AuthService
+  constructor(public auth: AuthService) {}
 
   login(): void {
     this.auth.loginWithRedirect({
       appState: {
-        target: '/todos' // Alvo após o login
-      }
-    }); // Método para redirecionar para o login
+        target: "/todos",
+      },
+    });
   }
 }
