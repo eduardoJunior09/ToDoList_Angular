37e6e79f4a20d175c11db8eab5e043a4843f44bc
eduardoJunior09 <eduardojunior.dev@gmail.com>
2024-11-03 19:29:43 -0300

    feat: add provideHttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,14 @@ import { provideRouter } from "@angular/router";
 import { routes } from "./app.routes";
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 import { provideAuth0 } from "@auth0/auth0-angular"; 
+import { provideHttpClient } from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideHttpClient(),
     provideAuth0({
       domain: 'dev-fg6i1mf6amp47jx5.us.auth0.com',  
       clientId: 'cLX7IksQ4n58pigtNVLRtoWx6UbfQuLy',
