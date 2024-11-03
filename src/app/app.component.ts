2bfbbd019694cf4d5c19f392425f92653a647d93
eduardoJunior09 <eduardojunior.dev@gmail.com>
2024-11-02 22:59:12 -0300

    Configuração do Auth0 e implementação inicial do sistema de autenticação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodoComponent } from './todo/todo.component';
+import { AuthService } from '@auth0/auth0-angular';
+import { CommonModule } from '@angular/common'; // Importe o CommonModule
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TodoComponent],
+  imports: [RouterOutlet, TodoComponent, CommonModule], // Adicione CommonModule aqui
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'lista-de-compras';
+
+  constructor(public auth: AuthService) {}
+
 }
