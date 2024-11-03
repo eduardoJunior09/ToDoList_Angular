9464c1a37c04adb8036ab6d744ea96344e7fee07
eduardoJunior09 <eduardojunior.dev@gmail.com>
2024-11-03 14:17:21 -0300

    refactor: melhorar tratamento de conexão com as informações do usuário

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-import { NewItemComponent } from '../components/new-item/new-item.component';
-import { DropDownListComponent } from '../features/drop-down-list/drop-down-list.component';
+import { Component, OnInit } from "@angular/core";
+import { NewItemComponent } from "../components/new-item/new-item.component";
+import { DropDownListComponent } from "../features/drop-down-list/drop-down-list.component";
+import { AuthService } from "@auth0/auth0-angular";
+import { User } from "@auth0/auth0-angular";
 
 @Component({
-  selector: 'app-todo',
+  selector: "app-todo",
   standalone: true,
   imports: [NewItemComponent, DropDownListComponent],
-  templateUrl: './todo.component.html',
-  styleUrl: './todo.component.scss',
+  templateUrl: "./todo.component.html",
+  styleUrl: "./todo.component.scss",
 })
-export class TodoComponent {}
+export class TodoComponent implements OnInit {
+  profile?: User | undefined | null;
+  constructor(public auth: AuthService) {}
+
+  ngOnInit(): void {
+    this.auth.user$.subscribe((profile) => {
+      this.profile = profile;
+    });
+  }
+}
