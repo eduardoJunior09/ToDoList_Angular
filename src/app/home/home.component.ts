02f25431e5213545625964c9954300ed83f3ff3d
eduardoJunior09 <eduardojunior.dev@gmail.com>
2024-11-06 00:01:26 -0300

    feat: adicionar login com Auth0 e gerenciamento de usuário

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,9 @@
-import { Component } from "@angular/core";
+// home.component.ts
+import { Component, OnInit } from "@angular/core";
 import { AuthService } from "@auth0/auth0-angular";
+import { TaskService } from "../service/task.service";
+import { UserProfile } from "../service/userProfile";
+import { UserSessionService } from "./../service/UserSessionServe";
 
 @Component({
   selector: "app-home",
@@ -7,14 +11,42 @@ import { AuthService } from "@auth0/auth0-angular";
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"],
 })
-export class HomeComponent {
-  constructor(public auth: AuthService) {}
+export class HomeComponent implements OnInit {
+  user: UserProfile | null = null;
+
+  constructor(
+    public auth: AuthService,
+    private taskService: TaskService,
+    private userSessionService: UserSessionService
+  ) {}
+
+  ngOnInit(): void {
+    this.auth.user$.subscribe((userProfile) => {
+      if (userProfile) {
+        this.user = userProfile;
+        this.addUser(userProfile);
+      }
+    });
+  }
 
   login(): void {
     this.auth.loginWithRedirect({
       appState: {
         target: "/todos",
       },
+    });
+  }
+
+  private addUser(userProfile: any): void {
+    const newUser: Omit<UserProfile, 'id'> = {
+      name: userProfile.name || "", 
+      email: userProfile.email || "",
+    };
+    this.taskService.addUser(newUser).subscribe((user) => {
+      if (user['id']) {
+        this.userSessionService.setUser(user); // Armazena o perfil completo do usuário no singleton
+        console.log("Usuário armazenado no Singleton:", this.userSessionService.getUser());
+      }
     });
   }
 }
